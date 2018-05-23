e69d61d9bd232bb14ad97e408dd7c9909f39379e
Suyash Deshpande <suyash.champ1@gmail.com>
2018-05-24 00:02:47 +0530

    Basics done using hbs

---
+++ db.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize')
+
+const db = new Sequelize(
+	'userdb',
+	'userdb',
+	'userdb',
+
+	{
+		dialect: 'mysql',
+		host: 'localhost'
+	}
+	)
+
+const Users = db.define('users',{
+	username:{
+		type:Sequelize.STRING,
+		allowNull:false,
+		unique:true
+	},
+	password: {
+		type:Sequelize.STRING,
+		allowNull:true
+	},
+	firstName:Sequelize.STRING,
+	lastName: Sequelize.STRING
+})
+
+db.sync().then(()=>{
+    console.log("Database created successfully")
+})
+
+exports = module.exports = {
+	db,
+	Users
+}
